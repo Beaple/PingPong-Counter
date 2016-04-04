b967498055e50fd3ed5a7fd1632a9b66a7d09ea7
Beaple <mail@philipgautschi.ch>
2016-04-04 02:07:38 +0200

    Update PingPong.ino
    fixed bug which doesnt display winning score

--- PingPong.ino
+++ PingPong.ino
@@ -96,7 +96,8 @@ void loop() // Hauptprogramm
 
             break;
           }
-
+          
+          display(points_L, points_R, !prevPoint_L, prevPoint_L); // Anzeige auf der Tafel
 
 
           if((points_L >= 11 or points_R >= 11) and abs(points_L - points_R) >= 2) // Sieg wenn ein Spieler mehr als 11 Punkte hat UND die Punktdifferenz midestens 2 ist
@@ -124,9 +125,10 @@ void loop() // Hauptprogramm
             
             points_L = 0; // Punkte fuer beide Spieler zurueck setzen
             points_R = 0;
+            
+            display(points_L, points_R, !prevPoint_L, prevPoint_L); // Anzeige auf der Tafel
           }
           
-          display(points_L, points_R, !prevPoint_L, prevPoint_L); // Anzeige auf der Tafel
           prevMillis = millis(); // Zeit der letzten Ausfuehrung speichern
   }
 }
