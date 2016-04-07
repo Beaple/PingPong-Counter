764d863dfe167011d6cde7517f8b845d06fbbf94
Beaple <mail@philipgautschi.ch>
2016-04-07 11:18:53 +0200

    Update sounds.ino

--- sounds.ino
+++ sounds.ino
@@ -1,4 +1,24 @@
 void victorySound()
 {
+  tone(pinSpeaker, 500, 250);
+  delay(250);
+  
+  tone(pinSpeaker, 800, 250);
+  delay(250);
+  
+  tone(pinSpeaker, 1000, 500);
+  delay(500);
+  
+  tone(pinSpeaker, 1200, 500);
+  delay(500);
+  
+  delay(100);
 
+  tone(pinSpeaker, 1200, 500);
+  delay(500);
+  
+  delay(100);
+  
+  tone(pinSpeaker, 1600, 500);
+  delay(500);
 }
